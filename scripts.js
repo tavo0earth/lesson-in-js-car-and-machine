c703ee22a9b6b4ed6f056d57c36b5776603841cd
tavo0earth <tavo0earth@gmail.com>
2019-07-07 00:10:50 +0300

    Fixed lesson car - try 1

--- scripts.js
+++ scripts.js
@@ -31,66 +31,58 @@
 */
 
 class CarCharacteristic {
-    constructor(engine, tank, fuelConsumption, fuelNow) {
-        this.engine = engine;
+    constructor(tank, fuelConsumption) {
         this.tank = tank;
         this.fuelConsumption = fuelConsumption;
-        this.fuelNow = fuelNow;
+        this.fuelNow = 0;
     }
 
-    tankCheck () {
-            if (this.fuelNow < 0) {
-                return "Значение должно быть положительным";
-            }
-            if (this.fuelNow > this.tank) {
-                return "Нельзя залить больше, чем " + this.tank + "л";
-            }
-        };
-
-    tankFull () {
-        if (this.fuelNow > 0 && this.fuelNow < this.tank) {
-            return true;
-        }
+    isEngineStarted () {
+        this.engine = true;
+        return this.engine
     }
 
-    motor () {
-        if (this.engine === true) {
-            return true
+    isEngineStopped () {
+        this.engine = false;
+        return this.engine
+    }
+
+
+    setFuelNow(tank) {
+        if (tank > 0 && tank < this.tank && typeof tank === "number") {
+            this.fuelNow = tank;
         } else {
-            return false
+            this.fuelNow = 'error';
         }
+    };
+
+    getFuelNow() {
+        return this.fuelNow;
+    };
+
+    timeGo () {
+        this.time = this.getFuelNow() / this.fuelConsumption;
+        alert('Машина проедет: ' + this.time + 'сек.');
+    }
+
+    timer () {
+        setTimeout(this.timeGo (), this.time () * 1000);
     }
 
     go() {
-        if (this.motor() === true && this.tankFull() === true) {
-            const time = this.fuelNow / this.fuelConsumption;
-            alert('Поехали!');
-            alert("Двигатель: " + this.motor());
-            alert('Бак заполнен на ' + this.fuelNow + "л.");
-            alert("Машина проедет: " + time + "с.")
-        }
-        if (this.motor() !== true) {
-            alert("Двигатель: " + this.motor());
-        }
-        if (this.tankFull() !== true) {
-            alert(this.tankCheck());
-        }
+        alert('Двигатель: ' + this.isEngineStarted());
+        alert('Бак заправлен на ' + this.getFuelNow() + 'л.');
+        alert(this.timer ());
+        alert(this.isEngineStopped());
     }
 
     stop() {
-        const time = 0;
-        if (this.motor() === true) {
-            alert("Двигатель: " + this.motor());
-            alert("Двигатель выключили");
-            alert("Машина проедет: " + time + "с.");
-        }
-        if (this.motor() !== true) {
-            alert("Двигатель: " + this.motor());
-        }
+        alert(this.isEngineStopped());
     }
 }
 
-const car = new CarCharacteristic(true, 200, 5, 100);
+const car = new CarCharacteristic(200, 5);
+car.setFuelNow(50);
 
 car.go();
 car.stop();
